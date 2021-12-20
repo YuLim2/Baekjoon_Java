d31cc5b9a0f9bd51c2bd81771233a8ea4ee4de13
YuLim2 <jyulim0120@gmail.com>
2021-12-21 01:55:31 +0900

    곱셈

--- Baekjoon_Java/src/week01/Baekjoon_2588.java
+++ Baekjoon_Java/src/week01/Baekjoon_2588.java
@@ -7,7 +7,7 @@ public class Baekjoon_2588 {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
-		int n1 = in.nextInt();
+		int n1 = in.nextInt(); 
 		String n2 = in.next();
 		
 		System.out.println(n1 * (n2.charAt(2) - '0'));
