f7acbddd5c8db73f445270ea7b57d283921fb387
YuLim2 <jyulim0120@gmail.com>
2021-12-21 01:54:16 +0900

    환경설정

---
+++ Baekjoon_Java/src/week01/Baekjoon_2588.java
@@ -0,0 +1,19 @@
+package week01;
+
+import java.util.Scanner;
+
+public class Baekjoon_2588 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int n1 = in.nextInt();
+		String n2 = in.next();
+		
+		System.out.println(n1 * (n2.charAt(2) - '0'));
+		System.out.println(n1 * (n2.charAt(1) - '0'));
+		System.out.println(n1 * (n2.charAt(0) - '0'));
+		System.out.println(n1 * Integer.parseInt(n2));
+	}
+
+}
