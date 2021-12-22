16b07dfb6ec1a20295b7321fbf35f4e45c9ecedd
YuLim2 <jyulim0120@gmail.com>
2021-12-22 15:26:07 +0900

    1주차 스터디

--- Baekjoon_Java/src/week01/Baekjoon_2588.java
+++ Baekjoon_Java/src/week01/Baekjoon_2588.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class Baekjoon_2588 {
 
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		
-		int n1 = in.nextInt(); 
-		String n2 = in.next();
+		int n1 = sc.nextInt(); 
+		String n2 = sc.next();
 		
 		System.out.println(n1 * (n2.charAt(2) - '0'));
 		System.out.println(n1 * (n2.charAt(1) - '0'));
